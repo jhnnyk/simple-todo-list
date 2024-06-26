48fbcc4ddd278afd9977f0e987462f248c4977e6
John Knotts <jhnnyk@gmail.com>
2024-06-26 12:49:10 -0500

    adding todos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,47 @@
-<script setup></script>
+<script setup>
+import { todosRef } from '@/firebase/config'
+import { addDoc } from 'firebase/firestore'
+import { Timestamp } from 'firebase/firestore'
+import { useCurrentUser } from 'vuefire'
+const user = useCurrentUser()
+
+const newTodo = defineModel('newTodo')
+
+const addNewTodo = async (e) => {
+  e.preventDefault()
+  console.log(newTodo.value)
+
+  try {
+    const createdAt = Timestamp.fromDate(new Date())
+    const docRef = await addDoc(todosRef, {
+      task: newTodo.value,
+      createdAt: createdAt
+    })
+    console.log(docRef)
+  } catch (err) {
+    console.log(err)
+  }
+
+  newTodo.value = ''
+}
+</script>
 
 <template>
   <main class="home">
-    <h1>welcome to the home page</h1>
+    <h1>ToDos</h1>
+    <div v-if="!user">
+      <p>Please Sign In or Create an Account</p>
+    </div>
+
+    <div v-else>
+      <p>{{ user ? user : '' }}</p>
+
+      <form @submit="addNewTodo">
+        <label>
+          <input v-model="newTodo" type="text" placeholder="add new todo" />
+        </label>
+        <button>+</button>
+      </form>
+    </div>
   </main>
 </template>
